6d71b07d0466e7d270b089f512ba6fa9cc0ce17c
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-23 08:13:20 -0500

    Constructores implementados y correciones

--- Ejercicio03/Node.java
+++ Ejercicio03/Node.java
@@ -5,8 +5,26 @@ public class Node<T> {
   private int balanceFactor;
   public Node(){
     this.data = null;
-    this.rightNode = null;
     this.leftNode = null;
-    this.balanceFactor = 0
+    this.rightNode = null;
+    this.balanceFactor = 0;
+  }
+  public Node(T data){
+    this.data = data;
+    this.leftNode = null;
+    this.rightNode = null;
+    this.balanceFactor = 0;
+  }
+  public Node(T data, Node<T> lefttNode){
+    this.data = data;
+    this.leftNode = leftNode;
+    this.rightNode = null;
+    this.balanceFactor = 0;
+  }
+  public Node(T data, Node<T> leftNode, Node<T> rightNode){
+    this.data = data;
+    this.leftNode = leftNode;
+    this.rightNode = rightNode;
+    this.balanceFactor = 0;
   }
 }
