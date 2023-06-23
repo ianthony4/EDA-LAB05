d385bd8e1786ec4267658e001ce3227f2eac2182
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-23 10:41:58 -0500

    metodo para saber si está vacío creado y prueba de insertar

--- Ejercicio03/BST.java
+++ Ejercicio03/BST.java
@@ -1,8 +1,15 @@
 public class BST<T extends Comparable<T>>{
   public Node<T> root;
 
+  // constructor
   public BST(){
     this.root = null;
+  }
+  public boolean isEmpty(){
+    return this.root == null;
+  }
+  public void insert(T data){
+    this.root = new Node<T>(data);
   }
 
 }
