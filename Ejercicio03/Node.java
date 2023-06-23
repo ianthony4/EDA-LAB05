b46dd6a194e4f413d4945337bab5098415128060
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-23 08:08:25 -0500

    Generismo implementado y constructor definido

--- Ejercicio03/Node.java
+++ Ejercicio03/Node.java
@@ -1,9 +1,12 @@
-public class Node{
+public class Node<T> {
   private T data;
   private Node rightNode;
   private Node leftNode;
   private int balanceFactor;
   public Node(){
-
+    this.data = null;
+    this.rightNode = null;
+    this.leftNode = null;
+    this.balanceFactor = 0
   }
 }
