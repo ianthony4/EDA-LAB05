525a3b700107428ec0424e07f18c5efce4c197f4
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-29 17:09:17 -0500

    Inicializando las clases, con atributos y constructores pequeños

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        //Todo el procedemiento para ejecutar el programa
+        //Ingresar la palabra
+        //Testear
+        //ETC
+    }
 }
--- NodeAVL.java
+++ NodeAVL.java
@@ -1,3 +1,14 @@
-public class NodeAVL {
+public class NodeAVL<T> {
+    //Datos del nodo
+    T date;
+    int alt;
+    NodeAVL<T> left;
+    NodeAVL<T> right;
+    NodeAVL<T> father;
     
+    //Iniciamos un nodo
+    NodeAVL(T dat) {
+        this.date = dat;
+        this.alt = 1;
+    }
 }
