94b31ed32dc2076a104f2f9b79fa0b50cb3fdc66
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-29 23:56:23 -0500

    Incializando la clase TreeGraph : Encargada de graficar

--- AVLTreeGraph.java
+++ AVLTreeGraph.java
@@ -1,12 +1,16 @@
 //Para graficar
-//import org.graphstream.graph.Graph;
-//import org.graphstream.graph.Node;
-//import org.graphstream.graph.implementations.SingleGraph;
-//import org.graphstream.ui.view.Viewer;
-//import org.graphstream.ui.*;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.view.Viewer;
 public class AVLTreeGraph {
     private AVLTree<Integer> tree;
-    //private Graph graph;
+    private Graph graph;
+
+    public AVLTreeGraph(AVLTree<Integer> tree) {
+        this.tree = tree;
+        this.graph = new SingleGraph("AVL TREE");
+    }
     
     
 }
