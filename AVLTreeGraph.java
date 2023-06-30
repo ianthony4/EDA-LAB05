5ccdc6aa1a72cece2aaad2bbc2873bbb7eb6e267
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-30 03:48:30 -0500

    Cambios para manejar más nodos

--- AVLTreeGraph.java
+++ AVLTreeGraph.java
@@ -12,37 +12,36 @@ public class AVLTreeGraph {
         this.graph = new SingleGraph("AVL TREE");
     }
     
-    private void addNode(NodeAVL<Integer> node, Node fatherNode) {
+    private void addNode(NodeAVL<Integer> node, Node padreNode, double x, double y, double separacion) {
         //creando nodo actual a trabajar
-        Node currentNode = graph.addNode(String.valueOf((char) node.date.intValue())); 
+        Node nodoActual = graph.addNode(String.valueOf((char) node.date.intValue())); 
         //ASCII A CHAR
-        currentNode.setAttribute("ui.label", Character.toString((char) node.date.intValue()));
-        currentNode.setAttribute("ui.style", "text-size: 60px;");
-        currentNode.setAttribute("layout.weight",0);
+        nodoActual.setAttribute("ui.label", Character.toString((char) node.date.intValue()));
+        nodoActual.setAttribute("ui.style", "text-size: 50px;");
+        nodoActual.setAttribute("layout.weight",0);
         //Si el nodo actual sera padre
-        if (fatherNode != null) {
-            graph.addEdge(fatherNode.getId() + currentNode.getId(), fatherNode.getId(), currentNode.getId());
-            currentNode.setAttribute("xy", 0, -1); //posicion xy
-            currentNode.setAttribute("layout.frozen");//estabilidad en el grafico 
+        if (padreNode != null) {
+            graph.addEdge(padreNode.getId() + nodoActual.getId(), padreNode.getId(), nodoActual.getId());
         }
-        //Si el nodo actual sera hijo izquierdo
-        if (node.left != null) {
-            addNode(node.left, currentNode);
-            currentNode.setAttribute("xy", 1, 0);
-            currentNode.setAttribute("layout.frozen");
-        }
-        //Si el nodo actual sera hijo derecho
+
+        nodoActual.setAttribute("xy", x, y);
+
         if (node.right != null) {
-            addNode(node.right, currentNode);
-            currentNode.setAttribute("xy", -1, 0);
-            currentNode.setAttribute("layout.frozen");
+            addNode(node.right, nodoActual, x + separacion, y - 1.0, separacion / 2);
+        }
+
+        if (node.left != null) {
+            addNode(node.left, nodoActual, x - separacion, y - 1.0, separacion / 2);
         }
     }
 
     public void printTree() {
-        System.setProperty("org.graphstream.ui", "swing");
-        addNode(tree.root, null);
+        addNode(tree.root, null, 0.0, 0.0, 4.0);
         Viewer viewer = graph.display();
+
+        Node nodoRaiz = graph.getNode(0);
+        nodoRaiz.setAttribute("ui.style", "fill-color: red;");
+        graph.setAttribute("layout.force", 1);
         viewer.disableAutoLayout();
     }
     
