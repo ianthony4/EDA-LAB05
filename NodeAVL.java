780c0463f6e76f11b00327ed80816f5c02bca201
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-30 03:51:10 -0500

    Getter and setters

--- NodeAVL.java
+++ NodeAVL.java
@@ -11,4 +11,35 @@ public class NodeAVL<T> {
         this.date = dat;
         this.alt = 1;
     }
+    //getters and setters
+    T getDate() {
+        return date;
+    }
+    public void setDate(T date) {
+        this.date = date;
+    }
+    public int getAlt() {
+        return alt;
+    }
+    public void setAlt(int alt) {
+        this.alt = alt;
+    }
+    public NodeAVL<T> getLeft() {
+        return left;
+    }
+    public void setLeft(NodeAVL<T> left) {
+        this.left = left;
+    }
+    public NodeAVL<T> getRight() {
+        return right;
+    }
+    public void setRight(NodeAVL<T> right) {
+        this.right = right;
+    }
+    public NodeAVL<T> getFather() {
+        return father;
+    }
+    public void setFather(NodeAVL<T> father) {
+        this.father = father;
+    }
 }
